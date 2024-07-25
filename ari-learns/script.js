3daaa9f1d5754744100632b494199ca87b1d3366
Ariharasudhan <ariharasudhan.s@zohocorp.com>
2024-07-25 22:13:09 +0530

    🍀

--- ari-learns/script.js
+++ ari-learns/script.js
@@ -1,11 +1,38 @@
-const div = document.getElementById("contents");
-let innerContents = '';
+document.addEventListener("DOMContentLoaded", function() {
+    const div = document.getElementById('contents');
+    let innerContents = '';
 
-fetch("https://raw.githubusercontent.com/arihara-sudhan/arihara-sudhan.github.io/main/ari-learns/meta.json")
-    .then(resp => resp.json())
-    .then(data=>{
-        data.learning_records.forEach(rec=>{
-            innerContents += `<a href="${rec.reference}"><div class="kural"><img src="${rec.img}" alt=""><span>${rec.title}</span></div></a>`
-        })
-        div.innerHTML = innerContents;
-})
+    fetch("https://raw.githubusercontent.com/arihara-sudhan/arihara-sudhan.github.io/main/ari-learns/meta.json")
+        .then(resp => resp.json())
+        .then(data => {
+            data.learning_records.forEach(rec => {
+                innerContents += `
+                    <a href="${rec.reference}" class="anchor">
+                        <div class="kural">
+                            <img src="${rec.img}" alt="">
+                            <span>${rec.title}</span>
+                            <span class="hoverElement" style="display: none;">${rec.description}</span>
+                        </div>
+                    </a>`;
+            });
+            div.innerHTML = innerContents;
+
+            const anchors = document.querySelectorAll('.anchor');
+
+            anchors.forEach(anchor => {
+                const hoverElement = anchor.querySelector('.hoverElement');
+
+                anchor.addEventListener('mouseover', () => {
+                    hoverElement.style.display = "block"
+                    hoverElement.style.fontSize = "1rem"
+                    hoverElement.style.border = "0.08rem solid black"
+                    hoverElement.style.padding = "0.5rem"
+                    hoverElement.style.borderRadius = "1rem"
+                });
+
+                anchor.addEventListener('mouseout', () => {
+                    hoverElement.style.display = 'none';
+                });
+            });
+        });
+});
