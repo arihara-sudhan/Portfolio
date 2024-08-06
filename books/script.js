871ae8e0e213baac7617437c92eb791701edab9c
Ariharasudhan <ariharasudhan.s@zohocorp.com>
2024-08-06 16:07:19 +0530

    BOOKS

--- books/script.js
+++ books/script.js
@@ -1,17 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
+    const tagsElement = document.getElementById("tags");
     const div = document.getElementById("contents");
-    let innerContents = '';
-
-    fetch("https://raw.githubusercontent.com/arihara-sudhan/arihara-sudhan.github.io/main/books/meta.json")
-        .then(resp => resp.json())
-        .then(data => {
-            data.forEach(book => {
-                innerContents += `<div class="book" onclick="window.open('${book.url}', '_blank');">
-                                    <img src="${book.cover}" alt="Book cover couldn't load">
-                                    <h3>${book.name}</h3>
-                                  </div>`;
+    let tagSelected = "";
+    
+    function createTagBanner(tags, container) {
+        tags.forEach(tag => {
+            const tagElement = document.createElement("h2");
+            tagElement.innerText = tag;
+            tagElement.addEventListener("click", () => {
+                tagSelected = tag;
+                updateContent();
             });
-            div.innerHTML = innerContents;
-        })
-        .catch(error => console.error('Error fetching the JSON file:', error));
+            container.appendChild(tagElement);
+        });
+    }
+
+    function updateContent() {
+        fetch("https://raw.githubusercontent.com/arihara-sudhan/arihara-sudhan.github.io/main/books/meta.json")
+            .then(resp => resp.json())
+            .then(data => {
+                let innerContents = '';
+                data.forEach(book => {
+                    if (tagSelected === "" || book.tag === tagSelected) {
+                        innerContents += `
+                            <div class="book" onclick="window.open('${book.url}', '_blank');">
+                                <img src="${book.cover}" alt="Book cover couldn't load">
+                                <h3>${book.name}</h3>
+                            </div>`;
+                    }
+                });
+                div.innerHTML = innerContents;
+            })
+            .catch(error => console.error('Error fetching the JSON file:', error));
+    }
+
+    const tags = ["web", "machine-learning", "tamizh", "general"];
+    createTagBanner(tags, tagsElement);
+    updateContent();
 });
