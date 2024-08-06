32813a24dd5e9e4b45855dfd1f03e618c1e969a1
Ariharasudhan <ariharasudhan.s@zohocorp.com>
2024-08-06 15:43:46 +0530

    BOOKS

--- books/script.js
+++ books/script.js
@@ -1,14 +1,17 @@
-const div = document.getElementById("contents");
-let innerContents = '';
+document.addEventListener('DOMContentLoaded', () => {
+    const div = document.getElementById("contents");
+    let innerContents = '';
 
-fetch("https://raw.githubusercontent.com/arihara-sudhan/arihara-sudhan.github.io/main/books/meta.json")
-    .then(resp => resp.json())
-    .then(data => {
-        data.forEach(book => {
-            innerContents += `<div class="book" onclick="window.load('${book.url}', '_blank');">
-            					<img src=${book.cover} alt="Book coudn't load">
-                                <h3>${book.name}</h3>
-                              </div>`;
-        });
-        div.innerHTML = innerContents;
-    });
+    fetch("https://raw.githubusercontent.com/arihara-sudhan/arihara-sudhan.github.io/main/books/meta.json")
+        .then(resp => resp.json())
+        .then(data => {
+            data.forEach(book => {
+                innerContents += `<div class="book" onclick="window.open('${book.url}', '_blank');">
+                                    <img src="${book.cover}" alt="Book cover couldn't load">
+                                    <h3>${book.name}</h3>
+                                  </div>`;
+            });
+            div.innerHTML = innerContents;
+        })
+        .catch(error => console.error('Error fetching the JSON file:', error));
+});
