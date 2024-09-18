493610f5dc5efbb5a9ccbe6aa536b00ed6e58d4b
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2024-09-18 08:31:57 +0530

    Update script.js

--- hobbies/script.js
+++ hobbies/script.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
             .catch(error => console.error('Error fetching the JSON file:', error));
     }
 
-    const tags = ["tamizh-poems", "stage", "book-review"] 
+    const tags = ["tamizh-poems", "stage", "book-review", "ichthyomania"]; 
     createTagBanner(tags, tagsElement);
     updateContent();
 });
