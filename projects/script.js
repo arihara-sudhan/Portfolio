1c90fee5ab034d5398beb11a8d69691ceac76743
Ariharasudhan <ariharasudhan.s@zohocorp.com>
2024-10-26 08:48:29 +0530

    PROJECTS REVAMP

--- projects/script.js
+++ projects/script.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
             .catch(error => console.error('Error fetching the JSON file:', error));
     }
 
-    const tags = ["tamizh-poems", "stage", "book-review", "phytophilia", "animalia", "aves", "insectidae"]; 
+    const tags = ["vision", "3d-modeling", "nlp", "drawing", "game-dev"]; 
     createTagBanner(tags, tagsElement);
     updateContent();
 });
