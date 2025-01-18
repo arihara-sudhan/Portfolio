57038721a7fa0120c9b719a514af4528358ef5ea
Ariharasudhan <ariharasudhan.s@zohocorp.com>
2025-01-18 07:01:34 +0530

    LANGUAGES REVAMP

--- resume/script.js
+++ resume/script.js
@@ -31,4 +31,31 @@ async function createProjects() {
   }
 }
 
+
+const LANGUAGES_API = 'https://raw.githubusercontent.com/arihara-sudhan/arihara-sudhan.github.io/refs/heads/main/resume/meta/languages.json';
+
+async function createLanguages() {
+  try {
+    const response = await fetch(LANGUAGES_API);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    const languages = await response.json();
+    const languagesContainer = document.querySelector('.languages');
+    let htmlContent = '';
+    languages.forEach(language => {
+      htmlContent += `
+        <div class="lang">
+          <img src="${language.image.src}" alt="${language.image.alt}">
+          <h2>${language.name}</h2>
+        </div>
+      `;
+    });
+    languagesContainer.innerHTML = htmlContent;
+  } catch (error) {
+    console.log(error, "ERROR CREATING LANGUAGES ARI! 🥴");
+  }
+}
+
+createLanguages();
 createProjects();
